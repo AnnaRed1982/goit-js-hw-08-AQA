9bbfdc68a5170ebdca24922e63de55f085c676ee
annared <annared1982@gmail.com>
2023-11-29 21:08:39 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const startBtn = document.querySelector('button[data-start]');
+// console.log('startBtn', startBtn);
+
+const stopBtn = document.querySelector('button[data-stop]');
+stopBtn.setAttribute("disabled", "true")
+// console.log('stopBtn', stopBtn);
