89b1f16ad77721f1a50bbac76a756dfb92179f23
annared <annared1982@gmail.com>
2023-11-29 21:24:25 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,9 +2,16 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+function setRandomBC() {
+  const bodyRef = document.querySelector('body');
+  return (bodyRef.style.backgroundColor = getRandomHexColor());
+}
+
 const startBtn = document.querySelector('button[data-start]');
 // console.log('startBtn', startBtn);
 
 const stopBtn = document.querySelector('button[data-stop]');
-stopBtn.setAttribute("disabled", "true")
+stopBtn.setAttribute('disabled', 'true');
 // console.log('stopBtn', stopBtn);
+
+startBtn.addEventListener('click', setRandomBC);
