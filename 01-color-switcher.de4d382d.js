4c89b7bb2abde069aa7cfec5d5eee9fbbecece3e
AnnaRed1982 <105372975+AnnaRed1982@users.noreply.github.com>
2023-11-29 19:49:28 +0000

    Deploying to gh-pages from @ AnnaRed1982/goit-js-hw-08-AQA@e5413571116ef07f3d7b381129eca8304427db15 🚀

--- 01-color-switcher.de4d382d.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]");document.querySelector("button[data-stop]").setAttribute("disabled","true"),t.addEventListener("click",(function(){return document.querySelector("body").style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}))}();
-//# sourceMappingURL=01-color-switcher.de4d382d.js.map
