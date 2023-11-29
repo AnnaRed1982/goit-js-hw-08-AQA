036972c6aacdf7cf334264ebdf848a52ebb10ee2
AnnaRed1982 <105372975+AnnaRed1982@users.noreply.github.com>
2023-11-29 20:03:44 +0000

    Deploying to gh-pages from @ AnnaRed1982/goit-js-hw-08-AQA@1f1d8010f81d2704466154eadef5448c9d3ca80c 🚀

--- 01-color-switcher.652a4b61.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.setAttribute("disabled","true");let r=null;t.addEventListener("click",(function(){const o=document.querySelector("body");e.removeAttribute("disabled"),t.setAttribute("disabled","true"),r=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),e.setAttribute("disabled","true"),clearInterval(r)}));
-//# sourceMappingURL=01-color-switcher.652a4b61.js.map
